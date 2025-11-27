00537dd9727e09ab553288ffd9762918a6a2af20
Douglas-Edson <douglas22@live.com.pt>
2025-11-27 17:39:33 -0300

    Exemplo Tipos Primitivos

---
+++ devdojo-logica/src/ExemploTiposPrimitivos.java
@@ -0,0 +1,22 @@
+public class ExemploTiposPrimitivos {
+    public static void main(String[] args) {
+        byte b = 100;
+        short s = 20000;
+        int i = 150000;
+        long l = 10000000000L;
+        float f = 3.14f;
+        double d = 3.14159265359;
+        char c = 'A';
+        boolean b1 = true;
+
+        System.out.println("byte: " + b);
+        System.out.println("Short: " + s);
+        System.out.println("int: " + i);
+        System.out.println("long: " + l);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("Char: " + c);
+        System.out.println("boolean: " + b1);
+
+    }
+}
